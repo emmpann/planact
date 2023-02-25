7391e3ea9cc8f83275ed6f575022bc499d36f898
emmpann <mhdepan@gmail.com>
2023-02-25 22:49:47 +0700

    updates page

--- app/src/main/java/com/efan/planact/MainActivity.kt
+++ app/src/main/java/com/efan/planact/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.onboarding_page)
+        setContentView(R.layout.login_page)
     }
 }
